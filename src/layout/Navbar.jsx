ca5652282d5240fdb25916faba4b7ae7235bd450
ap8114 <amanpatidar086@gmail.com>
2025-06-17 16:31:25 +0530

    pull

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,46 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Navbar = ({ toggleSidebar }) => {
   return (
     <>
-      <nav className="navbar navbar-light bg-card">
-        <div className="container-fluid nav-conte">
-          <div className="nav-content">
-            <div className="nav-bran">
-              <a className="nav-brand" href="#">
-               P.M.
+      <nav className="navbar navbar-expand-lg navbar-light bg-card">
+        <div className="container-fluid">
+          <div className="d-flex flex-grow-1 align-items-center">
+            {/* Brand and toggle */}
+            <div className="d-flex align-items-center">
+              <a className="navbar-brand me-2" href="#">
+                P.M.
               </a>
-              <div className="nav-taggle-icon" style={{fontSize:"25px",}} onClick={toggleSidebar}>
-                <a href="#">
-                  <i className="fa fa-bars" aria-hidden="true" style={{color:"white"}}></i>
-                </a>
-              </div>
+              <button 
+                className="navbar-toggler border-0 p-0" 
+                onClick={toggleSidebar}
+                style={{background: 'none', fontSize: "25px"}}
+              >
+                <i className="fa fa-bars text-white"></i>
+              </button>
             </div>
-            
-            {/* Added search bar in the center */}
-            <div className="search-bar">
+
+            {/* Search bar - will collapse on smaller screens */}
+            <div className="mx-lg-3 flex-grow-1 d-none d-md-block">
               <input 
                 type="text" 
                 placeholder="Search..." 
                 className="form-control"
-                style={{width: "500px", borderRadius:"30px"}}
+                style={{borderRadius: "30px", maxWidth: "500px", width: "100%"}}
               />
             </div>
-            
-            <div className="nav-main-icon">
-              <a className="bell-icon" href="#" style={{fontSize:"25px"}}>
+
+            {/* Right side icons */}
+            <div className="d-flex ms-auto">
+              {/* Notification bell - visible on all screens */}
+              <a className="nav-link text-white me-2 d-flex align-items-center" href="#" style={{fontSize: "25px"}}>
                 <i className="fa-regular fa-bell"></i>
               </a>
-              <div className="dropdown profile-elemen">
+
+              {/* Profile dropdown */}
+              <div className="dropdown">
                 <div
-                  className="me-2 fw-bold p-1 rounded-4 profile d-flex align-items-center"
-                  style={{ cursor: "pointer", fontSize:"25px" }}
+                  className="fw-bold rounded-4 d-flex align-items-center"
+                  style={{ cursor: "pointer", fontSize: "25px" }}
                   data-bs-toggle="dropdown"
-                  aria-expanded="false">
+                  aria-expanded="false"
+                >
                   <div className="profile-element">
                     <div className="avatar online">
-                      <i className="fa-solid user-icon fa-circle-user"></i>
-                      <span className="text-dark ms-2"></span>
+                      <i className="fa-solid user-icon fa-circle-user text-white"></i>
                     </div>
                   </div>
                 </div>
@@ -66,6 +74,16 @@ const Navbar = ({ toggleSidebar }) => {
                 </ul>
               </div>
             </div>
+          </div>
+
+          {/* Collapsible search bar for mobile */}
+          <div className="d-md-none w-100 mt-2">
+            <input 
+              type="text" 
+              placeholder="Search..." 
+              className="form-control w-100"
+              style={{borderRadius: "30px"}}
+            />
           </div>
         </div>
       </nav>
